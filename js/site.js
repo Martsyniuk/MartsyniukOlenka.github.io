91c6bcfb5af69af37dfc12c7f9cc37010ae96dcd
Martsyniuk <olena260695@ukr.net>
2017-09-29 10:29:14 +0300

    first commit

---
+++ js/site.js
@@ -0,0 +1,7 @@
+// $(Document).ready(function () {
+	function addColor3(){
+  		$("tr:even").css("background-color", "grey");
+  		$("tr:odd").css("background-color", "blue");
+  		$("td:gt(5)").css("color", "yellow");
+	}		
+// });
